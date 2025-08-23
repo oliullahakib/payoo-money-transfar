c6fbe06581477cabb4fbe68ef5a843eab0e5d0ce
oliullahakib <oliullahakib@gmail.com>
2025-08-23 12:15:45 +0600

    login condition added

--- js/login.js
+++ js/login.js
@@ -1,7 +1,20 @@
+let userPin =1234
 document.getElementById('login-btn')
 .addEventListener("click",function (e) {
     e.preventDefault();
+   let mobileNumber = document.getElementById("mobile-number").value;
+   let loginPin =parseInt(document.getElementById("login-pin").value) 
+   if(mobileNumber.length!==11 || !mobileNumber.startsWith("01")){
+    alert( "Put a Valid Mobile Number")
+    return
+   }
+   if(loginPin !==userPin){
+    alert('Pin Is Not Match');
+    return
+   }
     window.location.href = "./home.html"
+    document.getElementById("mobile-number").value = "";
+    document.getElementById("login-pin").value = "";
     
     
 })
