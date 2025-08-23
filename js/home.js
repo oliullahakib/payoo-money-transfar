3ad4d43c7b73ea79af54779910ce8ba812833422
oliullahakib <oliullahakib@gmail.com>
2025-08-23 16:33:16 +0600

    add money section and it's logicadded

---
+++ js/home.js
@@ -0,0 +1,34 @@
+let bankAccountNumber = 12345678901;
+let accountPin=1234;
+document.getElementById('add-money-btn').addEventListener("click", function (e) {
+    e.preventDefault()
+    let addAmount = parseInt(document.getElementById('add-amount').value);
+    let availableBlance = parseInt(document.getElementById("available-blance").innerText);
+    totalAmaount = addAmount + availableBlance;
+
+    let userAccountNumber = parseInt(document.getElementById("account-number").value);
+    let userPin = parseInt(document.getElementById("add-money-pin").value);
+    if(userAccountNumber !== bankAccountNumber){
+        alert("Invalid Account Number");
+        return
+    }
+    if(userPin !==accountPin){
+        alert('Invalid Pin');
+        return
+    }
+    let bank = document.getElementById('bank').value;
+   
+   if(bank === 'Seclect Bank'){
+    alert("Seclect a Bank To Add Money");
+    return
+   };
+    document.getElementById("available-blance").innerText = totalAmaount;
+
+    // reset all value
+    document.getElementById("add-money-pin").value = ''
+    document.getElementById("account-number").value = ''
+    document.getElementById('add-amount').value = ''
+    alert(`${addAmount}tk is added successfuly`);
+
+    
+})
