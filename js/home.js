50fc10289a6e00200bbed838f475a0dcf77084ed
oliullahakib <oliullahakib@gmail.com>
2025-08-25 16:21:51 +0600

    stand Out facality added and transactuibs section and its logics added

--- js/home.js
+++ js/home.js
@@ -16,15 +16,25 @@ function resetInput(id) {
     return document.getElementById(id).value = ''
 }
 
-  function viweSection(id) {
-      let sections = document.getElementsByClassName('section');
-    for(const section of sections){
-        section.style.display = "none"    
+function viweSection(id) {
+    let sections = document.getElementsByClassName('section');
+    for (const section of sections) {
+        section.style.display = "none"
     }
     document.getElementById(id).style.display = "block";
- }
+}
+
+function standOut(id) {
+    let cards = document.getElementsByClassName('card');
+    for (const card of cards) {
+        card.classList.add("border-gray-200", "text-gray-500", "bg-white");
+        document.getElementById(id).classList.remove("border-gray-200", "text-gray-500", "bg-white")
+    };
+    document.getElementById(id).classList.add("border-[#0874F2]", "text-blue-500", "bg-[#0874f20d]")
+}
 let bankAccountNumber = 12345678901;
 let accountPin = 1234;
+let transactionInfo = [];
 // logout section 
 document.getElementById('logout-btn').addEventListener("click", function (e) {
     e.preventDefault()
@@ -67,6 +77,11 @@ document.getElementById('add-money-btn').addEventListener("click", function (e) 
     document.getElementById('add-amount').value = ''
     alert(`${addAmount}tk is added successfuly`);
 
+    let transaction = {
+        name: "Add Money",
+        time: new Date().toLocaleTimeString()
+    };
+    transactionInfo.push(transaction);
 })
 
 // cashout section 
@@ -103,13 +118,15 @@ document.getElementById('withdraw-money-btn').addEventListener("click", function
     document.getElementById('withdraw-amount').value = ''
     alert(`${withdrawAmount}tk Cashout successfuly`);
 
+    let transaction = {
+        name: "Cash Out",
+        time: new Date().toLocaleTimeString()
+    };
+    transactionInfo.push(transaction);
+
 })
 // transfer money section 
-// document.getElementById("cash-out-card").addEventListener("click",function () {
-//     document.getElementById('cash-out-section').style.display = "block";
-//     document.getElementById('add-money-section').style.display = "none";
 
-// })
 document.getElementById('transfer-money-btn').addEventListener("click", function (e) {
     e.preventDefault()
     let transferAmount = getInputValueAsNumber('transfer-amount');
@@ -142,11 +159,15 @@ document.getElementById('transfer-money-btn').addEventListener("click", function
     document.getElementById('transfer-amount').value = ''
     alert(`${transferAmount}tk Transfer successfuly`);
 
+     let transaction = {
+        name: "Transfer Money",
+        time: new Date().toLocaleTimeString()
+    };
+    transactionInfo.push(transaction);
+
 })
 
 // pay bill section 
-
-
 // pay bill button
 document.getElementById('pay-bill-btn').addEventListener("click", function (e) {
     e.preventDefault()
@@ -156,7 +177,7 @@ document.getElementById('pay-bill-btn').addEventListener("click", function (e) {
         return
     }
     let availableBlance = getElementValueAsNumber("available-blance");
-    totalAmaount =  availableBlance - payAmount ;
+    totalAmaount = availableBlance - payAmount;
 
     let billAccountNumber = getInputValueAsNumber("bill-account-number");
     let userPin = getInputValueAsNumber("pay-bill-pin");
@@ -168,7 +189,7 @@ document.getElementById('pay-bill-btn').addEventListener("click", function (e) {
         alert('Invalid Pin');
         return
     }
-   let bank = document.getElementById("pay-bank").value;
+    let bank = document.getElementById("pay-bank").value;
     if (bank === 'Seclect Bank') {
         alert("Seclect a Bank To Add Money");
         return
@@ -180,45 +201,101 @@ document.getElementById('pay-bill-btn').addEventListener("click", function (e) {
     resetInput('pay-amount')
     alert(`${payAmount}tk is pay successfuly`);
 
+    let transaction = {
+        name: "Pay Bill",
+        time: new Date().toLocaleTimeString()
+    };
+    transactionInfo.push(transaction)
+
 })
 
 // get bonus section 
 document.getElementById("get-bonus-btn").addEventListener("click", function (e) {
     e.preventDefault()
     let coupon = "akib20";
-   let userCoupon = getInputValue("get-bonus-value");
-   if(userCoupon !== coupon){
-    alert('Invalid Coupon');
-    return
-   };
-   let rendomAmount = Math.floor(Math.random()*100);
-   let availableBlance = getElementValueAsNumber("available-blance");
-   totalAmaount =  availableBlance + rendomAmount ;
-   setBalance(totalAmaount);
-   alert(`${rendomAmount}tk bonus added successfuly`)
-   resetInput("get-bonus-value")
+    let userCoupon = getInputValue("get-bonus-value");
+    if (userCoupon !== coupon) {
+        alert('Invalid Coupon');
+        return
+    };
+    let rendomAmount = Math.floor(Math.random() * 100);
+    let availableBlance = getElementValueAsNumber("available-blance");
+    totalAmaount = availableBlance + rendomAmount;
+    setBalance(totalAmaount);
+    alert(`${rendomAmount}tk bonus added successfuly`)
+    resetInput("get-bonus-value")
+
+    let transaction = {
+        name: "Get Bonus",
+        time: new Date().toLocaleTimeString()
+    };
+    transactionInfo.push(transaction)
 })
 
+// Transactions section
+document.getElementById("transaction-card").addEventListener("click", function () {
+    let container = document.getElementById("history-card-container");
+    
+    container.innerHTML = "";
+    for (const info of transactionInfo) {
+    let div = document.createElement('div');
+    div.innerHTML = `
+     <div class="card flex flex-row items-center justify-between bg-white p-2">
+                    <div class="flex gap-3 items-center">
+                       <div class=" rounded-full p-2 bg-[#F4F5F7]"> <img src="./assets/purse1.png" alt=""></div>
+                        <div>
+                            <h4 class="font-semibold text-[#080808b3]">${info.name}</h4>
+                            <p class="text-gray-500">${info.time}</p>
+                        </div>
+                    </div>
+                    <i class="fa-solid fa-ellipsis-vertical"></i>
+                </div>
+    `
+    container.appendChild(div)
+}
+
+})
 // toggoling section
 document.getElementById("add-money-card").addEventListener("click", function (e) {
-   viweSection('add-money-section')
+    viweSection('add-money-section')
 });
 document.getElementById("cash-out-card").addEventListener("click", function () {
-   viweSection('cash-out-section')
+    viweSection('cash-out-section')
 
 });
 document.getElementById("transfer-money-card").addEventListener("click", function (e) {
-   viweSection('transfer-money-section')
+    viweSection('transfer-money-section')
 })
 document.getElementById("get-bonus-card").addEventListener("click", function (e) {
-   viweSection('get-bonus-section')
+    viweSection('get-bonus-section')
 })
 document.getElementById("pay-bill-card").addEventListener("click", function (e) {
-   viweSection('pay-bill-section')
+    viweSection('pay-bill-section')
 })
 document.getElementById("pay-bill-card").addEventListener("click", function (e) {
-   viweSection('pay-bill-section')
+    viweSection('pay-bill-section')
 })
 document.getElementById("transaction-card").addEventListener("click", function (e) {
-   viweSection('transaction-section')
+    viweSection('transaction-section')
 })
+
+// stand out 
+document.getElementById("add-money-card").addEventListener("click", function (e) {
+    standOut("add-money-card");
+});
+document.getElementById("cash-out-card").addEventListener("click", function (e) {
+    standOut("cash-out-card");
+});
+document.getElementById("transfer-money-card").addEventListener("click", function (e) {
+    standOut("transfer-money-card");
+});
+document.getElementById("get-bonus-card").addEventListener("click", function (e) {
+    standOut("get-bonus-card");
+});
+document.getElementById("pay-bill-card").addEventListener("click", function (e) {
+    standOut("pay-bill-card");
+});
+document.getElementById("transaction-card").addEventListener("click", function (e) {
+    standOut("transaction-card");
+});
+
